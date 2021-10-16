0e0b85d0dd0e32419aff533ff786dbdd70b26ae2
Daniel Tortosa Gonzalez <tortosad@ryanair.com>
2021-10-16 13:42:06 +0200

    Created ClassifiedAd

---
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAd
+    {
+        public Guid Id { get; }
+
+        public ClassifiedAd(Guid id, Guid ownerId)
+        {
+            if(id == default)
+                throw new ArgumentException("Identity must be specified", nameof(id));
+            if (ownerId == default)
+                throw new ArgumentException("Owner id must be specified", nameof(ownerId));
+
+            Id = id;
+            this.ownerId = ownerId;
+        }
+
+        public void SetTitle(string title) => this.title = title;
+
+        public void UpdateText(string text) => this.text = text;
+
+        public void UpdatePrice(decimal price) => this.price = price;
+
+        private Guid ownerId;
+        private string title;
+        private string text;
+        private decimal price;
+    }
+}
