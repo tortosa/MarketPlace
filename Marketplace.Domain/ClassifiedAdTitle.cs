9a41a21c7c1bbe09873b2b9ee57983ad5ca010c0
Daniel Tortosa Gonzalez <tortosad@ryanair.com>
2021-10-17 15:16:43 +0200

    Method factory to ClassifiedAdTitle

---
+++ Marketplace.Domain/ClassifiedAdTitle.cs
@@ -0,0 +1,29 @@
+﻿using Marketplace.Framework;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAdTitle : Value<ClassifiedAdTitle>
+    {
+        private const int maxLenght = 100;
+        private readonly string value;
+
+        public static ClassifiedAdTitle FromHtml(string htmlTitle)
+        {
+            var supportedTagsReplaced = htmlTitle.Replace("<i>", "*")
+                .Replace("<i>", "*")
+                .Replace("<i>", "*")
+                .Replace("<i>", "*");
+
+            return new ClassifiedAdTitle(Regex.Replace(supportedTagsReplaced, "<.*?>", string.Empty));
+        }
+
+        private ClassifiedAdTitle(string value)
+        {
+            if(value.Length > maxLenght)
+                throw new ArgumentOutOfRangeException($"Title cannot be longer than {maxLenght} characters", nameof(value));
+            this.value = value;
+        }
+    }
+}
